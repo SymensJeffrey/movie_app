e21216057aa7302c4b951561fb05d0d12229b7af
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-10-04 20:20:29 -0500

    remove static data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ import './App.css'
 import SearchIcon from './search.svg'
 
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`
-const movie1 = {
-    "Poster":"https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    "Title":"Shrek",
-    "Type":"movie",
-    "Year":"2001",
-    "imdbID":"tt0126029"
-}
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
@@ -23,7 +16,7 @@ const App = () => {
         setMovies(data.Search)
     }
     useEffect(() => {
-        searchMovies('Shrek')
+        searchMovies()
     }, []);
     
     return (
