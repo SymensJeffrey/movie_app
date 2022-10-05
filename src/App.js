2921eb5a3068e3d6456806319d27765a8229c168
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-10-04 19:39:18 -0500

    add static one movie

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css'
 import SearchIcon from './search.svg'
 
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`
+const movie1 = {
+    "Poster":"https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    "Title":"Shrek",
+    "Type":"movie",
+    "Year":"2001",
+    "imdbID":"tt0126029"
+}
 const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`)
@@ -32,8 +39,21 @@ const App = () => {
                 />
             </div>
 
-            <div>
-                
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
