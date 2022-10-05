ceda9de12ffc41ce40fd6aed10ba0755b8218e68
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-10-04 19:16:01 -0500

    add request to get movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import {useEffect} from "react";
 
+// e9cb0175
+
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data)
+    }
+    useEffect(() => {
+        searchMovies('Shrek')
+    }, []);
+    
     return (
         <h1>App</h1>
     );
